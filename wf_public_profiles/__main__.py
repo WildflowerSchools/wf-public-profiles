cd7314ccf04f24f5066450571c04192aa9cad6b2
Benjamin Jaffe-Talberg <ben.talberg@gmail.com>
2023-03-24 16:53:19 -0500

    Switch from APP_PORT to Heroku's internal env var PORT

--- wf_public_profiles/__main__.py
+++ wf_public_profiles/__main__.py
@@ -6,4 +6,4 @@ from . import log
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=settings.APP_PORT, log_config=log.UvicornLogger().dict())
+    uvicorn.run(app, host="0.0.0.0", port=settings.PORT, log_config=log.UvicornLogger().dict())
